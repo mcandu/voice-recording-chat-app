da489d2c7bb943c200a521fa0a0460c43e450825
mcandu <dundarmehmetcan@gmail.com>
2020-02-01 15:10:32 +0300

    media plugin tests for recording feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,16 +8,24 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { MediaCapture } from '@ionic-native/media-capture/ngx';
+import { Media } from '@ionic-native/media/ngx';
+import { File } from '@ionic-native/file/ngx';
+
+import { IonicStorageModule } from '@ionic/storage';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,IonicStorageModule.forRoot()],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    {provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    MediaCapture,
+    Media,
+    File
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
+import { NavController, Platform } from '@ionic/angular';
+import { Media, MediaObject } from '@ionic-native/media/ngx';
+import { File } from '@ionic-native/file/ngx';
+import { NgZone } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +11,89 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(
+    public navCtrl: NavController,
+    private media: Media,
+    private file: File,
+    public platform: Platform,
+    private cdr: ChangeDetectorRef) { }
+
+  recording: boolean = false;
+  filePath: string;
+  fileName: string;
+  audio: MediaObject;
+  audioList: any[] = [];
+  playing: any;
+
+  getAudioList() {
+    if (localStorage.getItem("audiolist")) {
+      this.audioList = JSON.parse(localStorage.getItem("audiolist"));
+      console.log(this.audioList);
+    }
+  }
+
+  ionViewWillEnter() {
+    this.getAudioList();
+  }
+
+  startRecord() {
+    if (this.platform.is('ios')) {
+      this.fileName = 'record' + new Date().getDate() + new Date().getMonth() + new Date().getFullYear() + new Date().getHours() + new Date().getMinutes() + new Date().getSeconds() + '.mp3';
+      this.filePath = this.file.documentsDirectory.replace(/file:\/\//g, '') + this.fileName;
+      this.audio = this.media.create(this.filePath);
+    } else if (this.platform.is('android')) {
+      this.fileName = 'record' + new Date().getDate() + new Date().getMonth() + new Date().getFullYear() + new Date().getHours() + new Date().getMinutes() + new Date().getSeconds() + '.mp3';
+      this.filePath = this.file.externalDataDirectory.replace(/file:\/\//g, '') + this.fileName;
+      this.audio = this.media.create(this.filePath);
+
+      // console.log(this.file.externalDataDirectory.replace(/file:\/\//g, ''));
+      // console.log(this.file.externalDataDirectory);
+      // console.log(this.file);
+      // console.log(this.filePath);
+    }
+    this.audio.startRecord();
+    this.recording = true;
+  }
+
+  stopRecord() {
+    this.audio.stopRecord();
+    let data = { filename: this.fileName };
+    this.audioList.push(data);
+    localStorage.setItem("audiolist", JSON.stringify(this.audioList));
+    this.recording = false;
+    this.getAudioList();
+  }
+
+  playAudio(file, index) {
+    this.stopAudio();
+    if (this.platform.is('ios')) {
+      this.filePath = this.file.documentsDirectory.replace(/file:\/\//g, '') + file;
+      this.audio = this.media.create(this.filePath);
+    } else if (this.platform.is('android')) {
+      this.filePath = this.file.externalDataDirectory.replace(/file:\/\//g, '') + file;
+      this.audio = this.media.create('https://www.w3schools.com/tags/horse.mp3');
+    }
+    this.playing = index;
+    this.audio.play();
+    this.audio.onSuccess.subscribe(() => {
+      // this.zone.run(() => {
+      //   this.playing = null;
+      // })
+      this.playing = null;
+      this.cdr.markForCheck();
+      this.cdr.detectChanges();
+    });
+    this.audio.setVolume(0.8);
+
+  }
+
+  stopAudio() {
+    if (this.audio) {
+      this.audio.stop();
+      this.playing = null;
+    }
+  }
+
+
 
 }
